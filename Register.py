94f7d65e1f6a16e0b0e7c053fc138878d71607a5
Hayden Hitt <>
2021-04-25 23:07:29 +0000

    CREATE Split.c

--- Register.py
+++ Register.py
@@ -1,16 +1,9 @@
 class Register:
-  def __init__(self,action,loc,subloc,origin):
-    self.action = str(action)
-    self.loc = str(loc)
-    self.subloc = str(subloc)
-    self.origin = str(origin)
-    self.opcode = str(str(action)+str(loc)+str(subloc)+str(origin))
-    
-  def register(self):
-    return 0
+  def __init__(self,roster):
+    self.roster = str(roster)
 
-  def postStream(self):
-    print(self.origin + self.action + " to register at " + self.subloc + " within "+ self.subloc)
+  def update(self):
+    import sqlite3 as db
 
-
-
+if __name__ == "__main__":
+  sample = Register()
